28aa4919fb74972d7cda243e182d492724ef3fab
lupin3000 <steffenlorenz@hotmail.de>
2021-03-20 11:18:24 +0100

    add ICMP ping

--- ICMPPing/ICMPping.py
+++ ICMPPing/ICMPping.py
@@ -1,3 +1,65 @@
 #!/usr/bin/env python
 
-pass
+import argparse
+import signal
+from sys import exit
+
+from scapy.layers.inet import IP, ICMP, sr, conf
+
+
+def keyboard_interrupt_handler(interrupt_signal, frame):
+    """
+    Keyboard (CTRL + C) interrupt function
+    """
+    print("Ping stopped")
+    print("KeyboardInterrupt ID: {} {} has been caught.".format(interrupt_signal, frame))
+    exit(0)
+
+
+def icmp_ping():
+    """
+    Ping function
+    """
+    global interface
+    global network
+
+    conf.iface = interface
+    conf.verb = 1
+    ans, uans = sr(IP(dst=network)/ICMP())
+    ans.summary(lambda s, r: r.sprintf("%IP.src% is alive"))
+
+
+def run_app():
+    """
+    Main function to parse arguments and run
+    """
+    global interface
+    global network
+
+    description = 'Simple ICMP ping'
+    epilog = 'The author of this code take no responsibility for your use or misuse'
+    parser = argparse.ArgumentParser(prog='ICMPping.py', description=description, epilog=epilog)
+    parser.add_argument("interface", help="Your interface")
+    parser.add_argument("network", help="Your target ip")
+    args = parser.parse_args()
+
+    if len(args.interface) < 1:
+        print('You did not provide any interface?')
+        exit(1)
+    else:
+        interface = args.interface
+
+    if len(args.network) < 1:
+        print('You did not provide any ip range?')
+        exit(1)
+    else:
+        network = args.network
+
+    print("Start ICMP ping on interface {} on network {}".format(interface, network))
+    icmp_ping()
+
+
+if __name__ == "__main__":
+    interface = network = None
+    signal.signal(signal.SIGINT, keyboard_interrupt_handler)
+    run_app()
--- ICMPPing/readme.md
+++ ICMPPing/readme.md
@@ -1,13 +1,38 @@
 # ICMP ping
 
-to be done ...
+Super simple ICMP ping implementation
 
 ## Prepare environment
 
-- to be done ...
+- You can use Python virtualenv
+- Python3.x is needed
+- root rights required (_or via sudo_)
 
 ```shell
-to be done ...
+# install needed packages
+$ sudo apt install python3-pip python3-venv
+
+# create and change into Project directory
+$ mkdir -p ~/Projects/ICMP-Ping && cd ~/Projects/ICMP-Ping
+
+# create virtualenv
+$ virtualenv -p python3 venv
+# or
+$ python3 -m venv venv
+
+# activate virtualenv
+$ . venv/bin/activate
+
+# install requirements
+(venv) $ pip install -r requirements.txt
+# or
+(venv) $ pip install scapy
+
+# list packages (optional)
+(venv) $ pip freeze
+
+# make file executable
+(venv) $ chmod u+x ICMPping.py
 ```
 
 ## Run ICMP ping
@@ -15,7 +40,11 @@ to be done ...
 just run it...
 
 ```shell
-to be done ...
+# show help (optional)
+(venv) $ sudo venv/bin/python ./ICMPping.py --help
+
+# scan for a specific IP
+(venv) $ sudo venv/bin/python ./ICMPping.py en11 172.20.10.1
 ```
 
 ## Example output
@@ -23,7 +52,12 @@ to be done ...
 some example from my terminal...
 
 ```shell
-to be done ...
+Start ICMP ping on interface en11 on network 172.20.10.1
+Begin emission:
+Finished sending 1 packets.
+
+Received 4 packets, got 1 answers, remaining 0 packets
+172.20.10.1 is alive
 ```
 
 [Go back](../README.md)
